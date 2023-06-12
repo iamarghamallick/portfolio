933599ba77e1327e5d7150b4a9087b6fc1e62211
Argha Mallick <arghamallick5@gmail.com>
2023-06-12 22:55:25 +0530

    first commit

---
+++ scripts/scrollReveal.js
@@ -0,0 +1,47 @@
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.about-right');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.about-left');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.skill-item');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.portfolio-item');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.contact-links');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.contact-form-wrapper');
